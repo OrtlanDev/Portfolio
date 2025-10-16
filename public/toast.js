c5ce27cf665e20877cd8e2ef10a4535bc056b05c
Orlandev <ortlandev@gmail.com>
2025-10-15 20:14:42 -0400

    feat: galería de imágenes implementada

--- public/toast.js
+++ public/toast.js
@@ -1,12 +1,12 @@
-import Toastify from "toastify-js";
-import "toastify-js/src/toastify.css";
+import Toastify from 'toastify-js';
+import 'toastify-js/src/toastify.css';
 
-window.showToast = function ({ text, type = "success" }) {
-    Toastify({
-        text,
-        duration: 4000,
-        gravity: "top",
-        position: "right",
-        backgroundColor: type === "success" ? "#22c55e" : "#ef4444",
-    }).showToast();
+window.showToast = function ({ text, type = 'success' }) {
+  Toastify({
+    text,
+    duration: 4000,
+    gravity: 'top',
+    position: 'right',
+    backgroundColor: type === 'success' ? '#22c55e' : '#ef4444',
+  }).showToast();
 };
